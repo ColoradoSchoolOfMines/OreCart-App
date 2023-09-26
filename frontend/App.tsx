b60ef7780b859b08fe4e71e39e736072429f87ea
Alexander Capehart <alex@oxycblt.org>
2023-09-26 17:50:34 -0600

    frontend/home: make background map
    Resolves #3.

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
+import MapView from 'react-native-maps';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from 'expo-status-bar';
+import * as NavigationBar from 'expo-navigation-bar';
+import { SafeAreaView } from 'react-native';
+
+NavigationBar.setBackgroundColorAsync("white")
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app! This works!!!!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <OreCart />
+    </SafeAreaView>
   );
+}
+
+function OreCart() {
+  return (
+    <View style={styles.main}>
+      <MapView style={styles.map} />
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1
+  },
+  main: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  map: {
+    width: '100%',
+    height: '100%'
+  }
 });
