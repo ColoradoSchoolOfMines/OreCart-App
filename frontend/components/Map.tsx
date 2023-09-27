0f174a83ba6ff2c227c18cac2afb9451228ef594
Alexander Capehart <alex@oxycblt.org>
2023-09-27 12:39:23 -0600

    frontend/main: add location indicator to map

--- frontend/components/Map.tsx
+++ frontend/components/Map.tsx
@@ -1,14 +1,20 @@
 import MapView from 'react-native-maps';
+import { Marker, Region, LatLng } from 'react-native-maps';
 import { StyleSheet, ViewStyle } from 'react-native';
+import { Coordinate } from '../services/location'
 
 export function Map(props: MapProps) {
   return (
-    <MapView style={styles.map} />
+    <MapView style={styles.map}
+      initialRegion={getNearbyRegion(props.currentLocation)}>
+      <Marker coordinate={asLatLng(props.currentLocation)} />
+    </MapView>
   )
 }
 
 export interface MapProps {
-  style: ViewStyle
+  style: ViewStyle,
+  currentLocation: Coordinate
 }
 
 const styles = StyleSheet.create({
@@ -17,3 +23,19 @@ const styles = StyleSheet.create({
     height: '100%'
   }
 })
+
+function getNearbyRegion(coord: Coordinate): Region {
+  return {
+    latitude: coord.lat,
+    longitude: coord.lon,
+    latitudeDelta: 0.002,
+    longitudeDelta: 0.002,
+  }
+}
+
+function asLatLng(coord: Coordinate): LatLng {
+  return {
+    latitude: coord.lat,
+    longitude: coord.lon
+  }
+}
--- frontend/screens/Main.tsx
+++ frontend/screens/Main.tsx
@@ -1,10 +1,13 @@
 import { StyleSheet, View } from 'react-native';
 import { Map } from '../components/Map'
+import { useState, useEffect } from 'react';
+import { Coordinate, getUserLocation } from '../services/location';
 
 export function Main() {
   return (
     <View>
-      <Map style={StyleSheet.absoluteFillObject} />
+      <Map style={StyleSheet.absoluteFillObject} 
+        currentLocation={{ lat: 39.749729, lon: -105.222740}} />
     </View>
   )
 }
