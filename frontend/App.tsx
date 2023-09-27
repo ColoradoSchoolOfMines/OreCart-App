d60490bdc9cbe159a9bfd2d5cccb842c34e48155
Alexander Capehart <alex@oxycblt.org>
2023-09-26 18:33:43 -0600

    frontend/home: zoom into and indicate location
    Currently based on hard-coded information.

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,25 +1,42 @@
 // import { StatusBar } from 'expo-status-bar';
 import MapView from 'react-native-maps';
+import { Marker, LatLng } from 'react-native-maps';
 import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
 import * as NavigationBar from 'expo-navigation-bar';
 import { SafeAreaView } from 'react-native';
+import { useState } from 'react';
 
 NavigationBar.setBackgroundColorAsync("white")
 
 export default function App() {
   return (
+    // TOOD/Stretch: Implement "true" edge-to-edge functionality (requires native code)
     <SafeAreaView style={styles.container}>
-      <OreCart />
+      <OreCartApp />
     </SafeAreaView>
   );
 }
 
-function OreCart() {
+function OreCartApp() {
+  let [coordinate, setCoordinate] = useState<LatLng>({ 
+    // TODO: Replace with user location
+    latitude: 39.7512546,
+    longitude: -105.2195490 }
+  )
+
   return (
     <View style={styles.main}>
-      <MapView style={styles.map} />
+      <MapView  style={styles.map} 
+                initialRegion={{
+                  latitude: coordinate.latitude,
+                  longitude: coordinate.longitude,
+                  latitudeDelta: 0.002,
+                  longitudeDelta: 0.002,
+                }}>
+        <Marker coordinate={coordinate} />
+      </MapView>
     </View>
   )
 }
