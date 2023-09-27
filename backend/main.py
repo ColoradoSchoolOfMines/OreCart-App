e802d6f8abf3548b3e994865797c2e4266e52615
Tyler Wright <tyler.wright5146@gmail.com>
2023-09-27 14:51:44 -0600

    Fastapi initial setup (#6)
    * quick fix to github actions. pylint needs other packages to be installed to not be mad
    
    * initial fastapi app with skeleton method for getting a van location
    
    * skeleton for post van location
    
    * blacken main.py

---
+++ backend/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class VanLocation(BaseModel):
+    lat: float
+    long: float
+    van_id: int
+    timestamp: int
+
+
+@app.get("/location/{van_id}")
+def get_van_location(van_id: int) -> VanLocation:
+    # get van location and return a proper VanLocation object
+
+    return VanLocation(lat=0.0, long=0.0, van_id=van_id, timestamp=0)
+
+
+@app.post("/location/{van_id}")
+def post_van_location(van_id: int, van_location: VanLocation):
+    # update van location in database
+
+    return {"message": "Location updated successfully."}
