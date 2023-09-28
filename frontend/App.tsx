4753c12bdcbd63c20396dae063247ed7150ec4db
Tyler Wright <tyler.wright5146@gmail.com>
2023-09-27 19:53:20 -0600

    package.json and package-lock.json + autofixes for eslint

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,15 +1,14 @@
-import { StyleSheet } from 'react-native';
-import * as NavigationBar from 'expo-navigation-bar';
-import { SafeAreaView } from 'react-native';
+import { StyleSheet, SafeAreaView } from 'react-native'
+import * as NavigationBar from 'expo-navigation-bar'
 import { Main } from './screens/Main'
 
 // TODO/Stretch: Implement "true" edge-to-edge functionality (requires native code)
-NavigationBar.setBackgroundColorAsync("white")
+NavigationBar.setBackgroundColorAsync('white')
 
-export default function App() {
+export default function App () {
   return (
     <SafeAreaView style={StyleSheet.absoluteFillObject}>
       <Main />
     </SafeAreaView>
-  );
+  )
 }
