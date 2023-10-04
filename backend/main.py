6980cbc9e9310de743fa4d198917644d21f82011
Cooper Nelson <cooper.richard222@gmail.com>
2023-10-03 18:56:02 -0600

    Create SQL that will create analytics table

--- backend/main.py
+++ backend/main.py
@@ -23,3 +23,16 @@ def post_van_location(van_id: int, van_location: VanLocation):
     # update van location in database
 
     return {"message": "Location updated successfully."}
+
+
+# TODO: Unsure of where this code should be located in the file
+create_analytics_sql = ("CREATE TABLE IF NOT EXISTS analytics ("
+"id INTEGER," # TODO: Maybe make this a PRIMARY KEY
+"van_id INTEGER," # Likely make this a foreign key, but potentially do later via ALTER
+"route_id INTEGER," # Likely make this a foreign key, but potentially do later via ALTER
+"entered INTEGER,"
+"exited INTEGER,"
+"lat DECIMAL(6,5),"
+"lon DECIMAL(6,5),"
+"ts TIMESTAMP"
+");")
