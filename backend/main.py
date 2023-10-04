3e13dfde94848eeb0bcfaa8c4d4aa8fbce18adb7
Tyler Wright <tyler.wright5146@gmail.com>
2023-10-03 19:39:08 -0600

    initial db stuff

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,26 @@
+import os
+import psycopg2
 from fastapi import FastAPI
 from pydantic import BaseModel
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
+
+@app.on_event("startup")
+def startup_event():
+    global conn, cursor
+
+
+    conn = psycopg2.connect(
+        database=os.getenv("database"),
+        host=os.getenv("db_host"),
+        user=os.getenv("db_user"),
+        password=os.getenv("db_password"),
+        port=os.getenv("db_port"),
+    )
+    cursor = conn.cursor()
 
 
 class VanLocation(BaseModel):
