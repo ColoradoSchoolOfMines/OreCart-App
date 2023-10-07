2137aeb47519eb10c351ca041996aaf982764d63
Alexander Capehart <alex@oxycblt.org>
2023-10-07 12:58:29 -0600

    Improve map location following (#23)
    * frontend/maps: improve location following
    
    The MapView camera now pans to user location through native MapView
    APIs, which makes it signifigantly faster and easier to manage.
    
    * frontend/map: do not snap to location if panning
    
    Do not move the camera to the user's location unless the user has
    panned around earlier.
    
    * frontend/map: only disable location on gesture
    
    Do not disable automatic location following on android unless the camera panning is actually
    caused by a user.
    
    This does not work on iOS, but iOS also has native location following, so it's okay.

--- frontend/src/Main.tsx
+++ frontend/src/Main.tsx
@@ -1,28 +1,13 @@
 import { StyleSheet, View } from 'react-native'
 import { Map } from './components/Map'
-import { useState, useEffect } from 'react'
-import { type Coordinate, subscribeUserLocation } from './services/location'
 
 /**
  * The main screen containing the map and bottom sheet pattern.
  */
 export function Main(): React.ReactElement<void> {
-  const [location, setLocation] = useState<Coordinate | null>(null)
-
-  useEffect(() => {
-    const promise = subscribeUserLocation(setLocation)
-    // Clean up memory once component unmounts
-    return () => {
-      promise
-        .then((watcher) => { watcher.remove() })
-        .catch(console.error)
-    }
-  }, [])
-
   return (
     <View>
-      <Map style={StyleSheet.absoluteFillObject}
-        currentLocation={location} />
+      <Map style={StyleSheet.absoluteFillObject} />
     </View>
   )
 }
