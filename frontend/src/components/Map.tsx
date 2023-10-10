aae43d2244d846e29f2e6ae97142d7a6dcec7c3f
Alexander Capehart <alex@oxycblt.org>
2023-10-10 17:14:18 -0600

    frontend/map: cleanup

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -18,7 +18,7 @@ export function Map(props: ViewProps): React.ReactElement<ViewProps> {
   const [userRegionChanged, setUserRegionChanged] = useState(false)
   const mapRef = useRef<MapView>(null)
 
-  function followUserLocationAndroid(location: Coordinate | undefined): void {
+  function panToLocation(location: Coordinate | undefined): void {
     // We want to make sure we won't snap back to the user location if they decide to pan around,
     // so check if that's the case before panning.
     if (location !== undefined && mapRef.current != null && !userRegionChanged)  {
@@ -32,24 +32,18 @@ export function Map(props: ViewProps): React.ReactElement<ViewProps> {
   return (
     <MapView style={styles.innerMap}
       ref={mapRef}
+      // Use Google Maps everywhere.
       provider={PROVIDER_GOOGLE}
       initialRegion={GOLDEN}
       showsUserLocation={true}
-      // Android only.
       showsMyLocationButton={false}
-      // followsUserLocation is only available on iOS, so we must reimplement the behavior on Android
-      // with onUserLocationChange.
-      followsUserLocation={!userRegionChanged}
-      onUserLocationChange={Platform.select({ 
-        android: event => { followUserLocationAndroid(event.nativeEvent.coordinate) } 
-      })}      
+      // followsUserLocation is only available on iOS maps, and isn't very cooperative anyway.
+      // Reimplement it ourselves.
+      onUserLocationChange={ event => { panToLocation(event.nativeEvent.coordinate) }}      
       onRegionChange={(_region, details) => {
-        // If the user is panning around, we don't want to snap back to their location.
-        // However, when we automatically pan to the location as part of the followsUserLocation
-        // reimplementation on android, this callback would be triggered and disable future pans.
-        // To prevent this, we check if the user is panning around by checking if the change was a
-        // gesture. This is not supported on iOS, but since we will only be panning the camera around
-        // on android, this is fine.
+        // If the user is panning around, we don't want to snap back to their location. Note that we
+        // make sure to exclude camera pans from this to avoid disabling location following at soon
+        // as it changes.
         if (details.isGesture ?? true) {
           setUserRegionChanged(true) 
         }
