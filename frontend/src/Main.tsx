9a7836d407fbfca5d43c1ba415e68b85d64ea7ae
Alexander Capehart <alex@oxycblt.org>
2023-10-10 18:39:32 -0600

    frontend: fix lint

--- frontend/src/Main.tsx
+++ frontend/src/Main.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet } from 'react-native'
 import { Map } from './components/Map'
 import { NavigationContainer } from '@react-navigation/native'
 
