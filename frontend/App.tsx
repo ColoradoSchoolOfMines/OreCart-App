1a3515ab89ec3063e569713350eba9e49f17078a
Alexander Capehart <alex@oxycblt.org>
2023-10-11 10:37:39 -0600

    frontend: add functioning drawer scaffold
    Requires even more baroque setup to not entirely crash.

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,3 +1,5 @@
+// REQUIRED FOR NAVIGATION. MUST BE IN THIS EXACT LOCATION. DO NOT REORDER.
+import 'react-native-gesture-handler';
 import { StyleSheet, SafeAreaView } from 'react-native'
 import * as NavigationBar from 'expo-navigation-bar'
 import { Main } from './src/Main'
--- frontend/src/Main.tsx
+++ frontend/src/Main.tsx
@@ -1,6 +1,9 @@
 import { StyleSheet } from 'react-native'
 import { Map } from './components/Map'
 import { NavigationContainer } from '@react-navigation/native'
+import { createDrawerNavigator } from '@react-navigation/drawer'
+
+const Drawer = createDrawerNavigator()
 
 /**
  * The main screen containing the map and bottom sheet pattern.
