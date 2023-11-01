0d5a802cb55d4c7eb05c36013dfe5bd7ffa337a4
Grant Lemons <grant0lemons@gmail.com>
2023-10-31 18:12:58 -0600

    change to fastapi state

--- backend/src/db.py
+++ backend/src/db.py
@@ -3,14 +3,12 @@ import os
 import sqlalchemy
 from sqlalchemy.orm import DeclarativeBase, Session
 
-engine = None
-
 
 def init():
-    global engine
     engine = sqlalchemy.create_engine(
         os.getenv("DATABASE_URL") or "", pool_size=15, max_overflow=5
     )
+    return engine
 
 
 class Base(DeclarativeBase):
--- backend/src/handlers/location.py
+++ backend/src/handlers/location.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
+from fastapi.orm import Request
 
 router = APIRouter(prefix="/location", tags=["location"])
 
@@ -12,14 +13,20 @@ class VanLocation(BaseModel):
 
 
 @router.get("/{van_id}")
-def get_van_location(van_id: int) -> VanLocation:
+def get_van_location(req: Request, van_id: int) -> VanLocation:
     # get van location and return a proper VanLocation object
+
+    # get engine from app state
+    engine = req.app.state.engine
 
     return VanLocation(lat=0.0, lon=0.0, van_id=van_id, timestamp=0)
 
 
 @router.post("/{van_id}")
-def post_van_location(van_id: int, van_location: VanLocation):
+def post_van_location(req: Request, van_id: int, van_location: VanLocation):
     # update van location in database
+
+    # get engine from app state
+    engine = req.app.state.engine
 
     return {"message": "Location updated successfully."}
