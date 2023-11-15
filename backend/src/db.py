ba02336d94994fb9dce181933baa6b1eeea13f5a
Ethan Richards <42894274+ezrichards@users.noreply.github.com>
2023-11-14 17:54:22 -0700

    Strongly type db.py and add docstrings

--- backend/src/db.py
+++ backend/src/db.py
@@ -1,15 +1,24 @@
 import os
-
 import sqlalchemy
-from sqlalchemy.orm import DeclarativeBase, Session
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.engine import Engine
 
 
-def init():
-    engine = sqlalchemy.create_engine(
-        os.getenv("DATABASE_URL") or "", pool_size=15, max_overflow=5
+def init() -> Engine:
+    """
+    Initializes a database engine that is passed to the
+    app state and can be accessed by req.app.state.engine
+    """
+    engine: Engine = sqlalchemy.create_engine(
+        os.environ["DATABASE_URL"], pool_size=15, max_overflow=5
     )
     return engine
 
 
 class Base(DeclarativeBase):
+    """
+    This makes an instance of DeclarativeBase which is the inherited model for all models.
+    This is an alternative to using declarative_base from sqlalchemy.orm and making 
+    a new instance of DeclarativeBase upon the creation of each new model.
+    """
     pass
