3352c2c9148f483f66b127033427781ad0569ee1
Alexander Capehart <alex@oxycblt.org>
2023-11-21 17:36:56 -0700

    backend/ridership: add post route
    Add a route for hardware to post an instance of ridership statistics to
    the backend.

--- backend/src/model/van.py
+++ backend/src/model/van.py
@@ -1,11 +1,12 @@
 from sqlalchemy import ForeignKeyConstraint
 from sqlalchemy.orm import Mapped, mapped_column
+
 from src.db import Base
 
 
 class VanModel(Base):
     __tablename__ = "vans"
-    __table_args__ = ForeignKeyConstraint(["route_id"], ["routes.id"])
+    __table_args__ = tuple(ForeignKeyConstraint(["route_id"], ["routes.id"]))
     id: Mapped[int] = mapped_column(
         primary_key=True, autoincrement=True, nullable=False
     )
