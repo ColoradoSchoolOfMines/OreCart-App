768994a8005ae353c5aa327828d7ad84e1acf8a5
Alexander Capehart <alex@oxycblt.org>
2023-11-22 19:17:26 -0700

    backend: more tweaks

--- backend/src/request.py
+++ backend/src/request.py
@@ -2,6 +2,11 @@ from fastapi import HTTPException
 
 
 def validate_include(include: list[str], allowed: set[str]):
+    """
+    Validates the include parameter of a request, ensuring that it contains no duplicates
+    and only includes valid fields.
+    """
+
     include_set = set(include)
     if len(include_set) != len(include):
         raise HTTPException(status_code=400, detail="Duplicate include parameters")
