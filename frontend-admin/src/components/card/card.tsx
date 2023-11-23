8575bbb82d31128e096782b6776954d8df6af2dd
shanecranor <shanecranor@gmail.com>
2023-11-22 19:33:39 -0700

    fix capitalization

--- frontend-admin/src/components/card/card.tsx
+++ frontend-admin/src/components/card/card.tsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import './Card.scss';
+import './card.scss';
 
 interface CardProps {
   title: string;
--- frontend-admin/src/components/navbar/navbar.tsx
+++ frontend-admin/src/components/navbar/navbar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '../../assets/logo.png';
-import './Navbar.scss';
+import './navbar.scss';
 interface NavBarProps { }
 
 const NavBar: React.FC<NavBarProps> = () => {
