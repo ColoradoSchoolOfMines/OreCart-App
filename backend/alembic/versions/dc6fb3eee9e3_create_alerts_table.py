1d6e0cac1362bafb29d1a91b7141d9d8c85417c7
Alexander Capehart <alex@oxycblt.org>
2023-11-24 11:29:41 -0700

    backend: timezone all db timestamp fields

--- backend/alembic/versions/dc6fb3eee9e3_create_alerts_table.py
+++ backend/alembic/versions/dc6fb3eee9e3_create_alerts_table.py
@@ -22,8 +22,8 @@ def upgrade() -> None:
         CREATE TABLE IF NOT EXISTS public.alerts (
 	        id serial PRIMARY KEY,
 	        text VARCHAR(500) NOT NULL,
-	        start_datetime timestamp NOT NULL,
-	        end_datetime timestamp NOT NULL CHECK (end_datetime > start_datetime)
+	        start_datetime timestamptz NOT NULL,
+	        end_datetime timestamptz NOT NULL CHECK (end_datetime > start_datetime)
         );
     """
     )
