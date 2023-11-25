52be6406cc8313b018618ff57dd886faed1e54a7
Tyler Wright <tyler.wright5146@gmail.com>
2023-11-25 00:33:57 -0700

    includes

--- backend/src/handlers/vans.py
+++ backend/src/handlers/vans.py
@@ -3,7 +3,9 @@ from typing import Dict, List, Union
 from fastapi import APIRouter, Query, Request
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
+from src.model.analytics import Analytics
 from src.model.van import Van
+from src.request import process_include
 
 
 class VanModel(BaseModel):
@@ -17,32 +19,90 @@ class VanModel(BaseModel):
 
 router = APIRouter(prefix="/vans", tags=["vans"])
 
+ANALYTICS_RIDERSHIP = "analytics-ridership"
+INCLUDES = {ANALYTICS_RIDERSHIP}
+
 
 @router.get("/")
 def get_vans(
     req: Request, include: Union[List[str], None] = Query(default=None)
 ) -> JSONResponse:
+    include_set = process_include(include=include, allowed=INCLUDES)
     with req.app.state.db.session() as session:
         vans: List[Van] = session.query(Van).all()
 
-    resp = {
-        "vans": [
-            {"van_id": van.id, "route_id": van.route_id, "wheelchair": van.wheelchair}
-            for van in vans
-        ]
-    }
+        resp = {
+            "vans": [
+                {
+                    "van_id": van.id,
+                    "route_id": van.route_id,
+                    "wheelchair": van.wheelchair,
+                }
+                for van in vans
+            ]
+        }
+
+        if ANALYTICS_RIDERSHIP in include_set:
+            analytics: List[Analytics] = session.query(Analytics).all()
+            analytics = list(
+                map(
+                    lambda analytic: {
+                        "id": analytic.id,
+                        "van_id": analytic.van_id,
+                        "route_id": analytic.route_id,
+                        "entered": analytic.entered,
+                        "exited": analytic.exited,
+                        "lat": analytic.lat,
+                        "lon": analytic.lon,
+                        "datetime": str(analytic.datetime),
+                    },
+                    analytics,
+                )
+            )
+            for van in resp["vans"]:
+                van["analytics-ridership"] = list(
+                    filter(analytics, lambda analytic: analytic.van_id == van.id)
+                )
 
     return JSONResponse(content=resp)
 
 
 @router.get("/{van_id}")
-def get_van(req: Request, van_id: int) -> JSONResponse:
+def get_van(
+    req: Request, van_id: int, include: Union[List[str], None] = Query(default=None)
+) -> JSONResponse:
+    include_set = process_include(include=include, allowed=INCLUDES)
     with req.app.state.db.session() as session:
         van: Van = session.query(Van).filter_by(id=van_id).first()
         if van is None:
             return JSONResponse(content={"message": "Van not found"}, status_code=404)
 
-    resp = {"van_id": van_id, "route_id": van.route_id, "wheelchair": van.wheelchair}
+        resp = {
+            "van_id": van_id,
+            "route_id": van.route_id,
+            "wheelchair": van.wheelchair,
+        }
+
+        if ANALYTICS_RIDERSHIP in include_set:
+            analytics: List[Analytics] = (
+                session.query(Analytics).filter_by(van_id=van.id).all()
+            )
+            analytics = list(
+                map(
+                    lambda analytic: {
+                        "id": analytic.id,
+                        "van_id": analytic.van_id,
+                        "route_id": analytic.route_id,
+                        "entered": analytic.entered,
+                        "exited": analytic.exited,
+                        "lat": analytic.lat,
+                        "lon": analytic.lon,
+                        "datetime": str(analytic.datetime),
+                    },
+                    analytics,
+                )
+            )
+            resp["analytics"] = analytics
 
     return JSONResponse(content=resp)
 
