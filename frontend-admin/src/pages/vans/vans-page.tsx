d68c6597c999be3b7616dc373953101b3e8f7a12
Tyler Wright <tyler.wright5146@gmail.com>
2023-11-27 16:08:08 -0700

    can make new van

--- frontend-admin/src/pages/vans/vans-page.tsx
+++ frontend-admin/src/pages/vans/vans-page.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { useQuery } from 'react-query';
+import React, { useState } from 'react';
+import { useQuery, useQueryClient } from 'react-query';
 import Card from '../../components/card/card';
 import './vans-page.scss';
 
@@ -7,6 +7,16 @@ interface Van {
   vanId: number;
   routeId: number;
   wheelchair: boolean;
+}
+
+interface VanData {
+  route_id: number;
+  wheelchair: boolean;
+  // file: File; // If you're including a file, though handling it in JSON is complex.
+}
+
+interface AddVanFormProps {
+  onSubmit: (data: VanData) => void; // or Promise<void> if async
 }
 
 const fetchVans = async () => {
@@ -17,7 +27,33 @@ const fetchVans = async () => {
 };
 
 const VanPage: React.FC = () => {
+  const queryClient = useQueryClient();
   const { data: vans, isLoading, error } = useQuery('vans', fetchVans);
+  const [isPopupOpen, setPopupOpen] = useState(false);
+
+  const handleAddVanClick = () => {
+    setPopupOpen(true);
+  };
+
+  const createVan = async (vanData: VanData) => {
+    // Convert the routeId to an integer
+    try {
+      console.log(JSON.stringify(vanData));
+      await fetch('http://localhost:8000/vans', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(vanData),
+      });
+      await queryClient.invalidateQueries('vans');
+      setPopupOpen(false);
+      // ... Handle the response ...
+    } catch (error) {
+      console.error('Error creating van:', error);
+    }
+  };
+  
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>An error occurred: {(error as Error).message}</div>;
@@ -30,8 +66,47 @@ const VanPage: React.FC = () => {
           <Card title={`Van #${van.vanId}`}></Card>
         ))}
       </div>
+
+      <button onClick={handleAddVanClick}>
+        Add Van
+      </button>
+      {isPopupOpen && <AddVanForm onSubmit={createVan} />}
     </main>
   );
 };
+
+const AddVanForm: React.FC<AddVanFormProps> = ({ onSubmit }) => {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const form = e.currentTarget;
+    const routeIdInput = form.elements.namedItem('routeId') as HTMLInputElement;
+    const wheelchairInput = form.elements.namedItem('wheelchair') as HTMLInputElement;  
+
+    const formData: VanData = {
+      route_id: parseInt(routeIdInput.value, 10),
+      wheelchair: wheelchairInput.checked,
+    };
+
+    onSubmit(formData);
+  };
+
+  return (
+    <div className="popup-form">
+      <form onSubmit={handleSubmit}>
+        <label>
+          Route ID:
+          <input type="number" name="routeId" required />
+        </label>
+        <label>
+          Wheelchair Accessible:
+          <input type="checkbox" name="wheelchair" />
+        </label>
+        <button type="submit">Create Van</button>
+      </form>
+    </div>
+  );
+};
+
 
 export default VanPage;
