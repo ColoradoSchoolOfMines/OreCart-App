6a2a76691f3114b3c5a5c8b9adf1205c43325676
Ethan Richards <42894274+ezrichards@users.noreply.github.com>
2023-12-19 07:35:13 -0700

    Remove unused React boilerplate

--- frontend-admin/src/main.tsx
+++ frontend-admin/src/main.tsx
@@ -1,7 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import App from './App.tsx'
 import {
   createBrowserRouter,
   RouterProvider,
