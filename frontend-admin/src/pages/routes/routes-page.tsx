c4a88d1f1cdb5c284fbf86ea2a0d98ac3547541d
Tyler Wright <tyler.wright5146@gmail.com>
2024-01-14 18:15:44 -0700

    No more editing routes, can clear routes.

--- frontend-admin/src/pages/routes/routes-page.tsx
+++ frontend-admin/src/pages/routes/routes-page.tsx
@@ -2,7 +2,6 @@ import { useQuery, useQueryClient } from '@tanstack/react-query';
 import React, { useRef, useState } from 'react';
 import Card from '../../components/card/card';
 import AddRouteForm from './add-route-form';
-import EditRouteForm from './edit-route-form';
 import { Route, RouteEditFormRef } from './route-types';
 import './routes-page.scss';
 
@@ -40,37 +39,9 @@ const RoutesPage: React.FC = () => {
     }
   };
 
-  const openRouteEditForm = (routeId: number) => {
-    setCurrentRouteId(routeId);
-    dialogEditRef.current?.showModal();
-    routeEditFormRef.current?.clearForm();
-    const route = routes?.find((route: Route) => route.id === routeId);
-    if (route) {
-      routeEditFormRef.current?.setData(route);
-    }
-  };
-
-  const handleEditFormSubmit = async (formData: FormData) => {
+  const clearRoutes = async () => {
     try {
-      const response = await fetch(`${baseUrl}/routes/${currentRouteId}`, {
-        method: 'PUT',
-        body: formData, // FormData is directly sent without setting Content-Type header
-      });
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      // Handle the successful response
-      await queryClient.invalidateQueries({ queryKey: ['routes'] });
-      setCurrentRouteId(-1);
-      dialogEditRef.current?.close();
-    } catch (error) {
-      console.error('There was a problem with the fetch operation:', error);
-    }
-  };
-
-  const handleDelete = async () => {
-    try {
-      const response = await fetch(`${baseUrl}/routes/${currentRouteId}`, {
+      const response = await fetch(`${baseUrl}/routes/`, {
         method: 'DELETE',
       });
       if (!response.ok) {
@@ -78,12 +49,10 @@ const RoutesPage: React.FC = () => {
       }
       // Handle the successful response
       await queryClient.invalidateQueries({ queryKey: ['routes'] });
-      setCurrentRouteId(-1);
-      dialogEditRef.current?.close();
     } catch (error) {
       console.error('There was a problem with the fetch operation:', error);
     }
-  };
+  }
 
 
   if (isLoading) return <div>Loading...</div>;
@@ -94,18 +63,20 @@ const RoutesPage: React.FC = () => {
       <h1>Routes</h1>
       <div className="cards-container">
         {routes?.map((route: Route) => (
-          <Card title={`${route.name} (${route.id})`} key={route.id} onClick={() => {openRouteEditForm(route.id)}}></Card>
+          <Card title={`${route.name} (${route.id})`} key={route.id}></Card>
         ))}
       </div>
 
-      <button onClick={() => {dialogRef.current?.show()}}>Add Route</button>
+      {
+        (!routes || routes?.length == 0) && <button onClick={() => {dialogRef.current?.show()}}>Add Route</button>
+      }
+
+      {
+        !(!routes || routes?.length == 0) && <button onClick={() => {clearRoutes()}}>Clear Routes</button>
+      }
 
       <dialog ref={dialogRef}>
         <AddRouteForm onSubmit={handleFormSubmit} onCancel={() => {dialogRef.current?.close()}} />
-      </dialog>
-
-      <dialog ref={dialogEditRef}>
-        <EditRouteForm ref={routeEditFormRef} onSubmit={handleEditFormSubmit} onCancel={() => {dialogEditRef.current?.close()}} onDelete={handleDelete} />
       </dialog>
 
     </main>
