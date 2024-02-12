bcdd5d882ed009249f4e2859f574d49242401f39
shanecranor <shanecranor@gmail.com>
2024-02-12 04:10:15 -0600

    added mantine and fixed modals

--- frontend-admin/src/main.tsx
+++ frontend-admin/src/main.tsx
@@ -1,18 +1,17 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {
-  RouterProvider,
-  createBrowserRouter,
-} from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./index.css";
-import AccommodationsPage from './pages/accommodations/accommodations-page';
-import AlertsPage from './pages/alerts/alerts-page';
-import RidershipPage from './pages/ridership/ridership-page';
-import RoutesPage from './pages/routes/routes-page';
-import VanPage from './pages/vans/vans-page';
-import Root from './templates/root';
+import AccommodationsPage from "./pages/accommodations/accommodations-page";
+import AlertsPage from "./pages/alerts/alerts-page";
+import RidershipPage from "./pages/ridership/ridership-page";
+import RoutesPage from "./pages/routes/routes-page";
+import VanPage from "./pages/vans/vans-page";
+import Root from "./templates/root";
 
+import { MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.layer.css";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -20,34 +19,36 @@ const router = createBrowserRouter([
     children: [
       {
         path: "/vans",
-        element: <VanPage />
+        element: <VanPage />,
       },
       {
         path: "/ridership",
-        element: <RidershipPage />
+        element: <RidershipPage />,
       },
       {
         path: "/routes",
-        element: <RoutesPage />
+        element: <RoutesPage />,
       },
       {
         path: "/accommodations",
-        element: <AccommodationsPage />
+        element: <AccommodationsPage />,
       },
       {
         path: "alerts",
-        element: <AlertsPage />
-      }
-    ]
+        element: <AlertsPage />,
+      },
+    ],
   },
 ]);
 
 const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <QueryClientProvider client={queryClient}>
     <React.StrictMode>
-      <RouterProvider router={router} />
+      <MantineProvider>
+        <RouterProvider router={router} />
+      </MantineProvider>
     </React.StrictMode>
-  </QueryClientProvider>,
+  </QueryClientProvider>
 );
