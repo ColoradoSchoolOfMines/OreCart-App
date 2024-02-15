ff9915a1bbcc72eda8bd94d8e856499e91cc1dd9
b-sharman <byron.n.sharman@gmail.com>
2024-02-14 17:23:11 -0700

    provide confirmation to the user when they log in

--- frontend-admin/src/pages/login/login-page.tsx
+++ frontend-admin/src/pages/login/login-page.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 
 const baseUrl = import.meta.env.VITE_BACKEND_URL;
 
+const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  document.getElementById("login-confirmation").style.display="block";
+}
+
 const LoginPage: React.FC = () => {
   return (
     <main>
 
       <h1>Log In</h1>
 
-      <form action={`${baseUrl}/auth/login`} method="post">
+      <form method="post" action={`${baseUrl}/auth/login`} onSubmit={handleSubmit}>
         <div>
           <label>Username:
             <input type="text" name="username" required />
@@ -23,6 +27,7 @@ const LoginPage: React.FC = () => {
           <input type="submit" value="Log in" />
         </div>
       </form>
+      <p id="login-confirmation" style={{"display": "none"}}>You have logged in successfully; other pages are now available!</p>
 
     </main>
   );
