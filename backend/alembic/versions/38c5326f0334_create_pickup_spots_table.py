1a9c9c3dc5c9339a749a277c803f5c33b158ed84
Alexander Capehart <alex@oxycblt.org>
2024-02-23 21:46:41 -0700

    backend: fix alembic revision

--- backend/alembic/versions/38c5326f0334_create_pickup_spots_table.py
+++ backend/alembic/versions/38c5326f0334_create_pickup_spots_table.py
@@ -1,7 +1,7 @@
 """create pickup spots table
 
 Revision ID: 38c5326f0334
-Revises: 1a8780dd5bbc
+Revises: d53db4ccb3fc
 Create Date: 2024-02-22 10:17:36.297413
 
 """
@@ -13,7 +13,7 @@ from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "38c5326f0334"
-down_revision: Union[str, None] = "1a8780dd5bbc"
+down_revision: Union[str, None] = "d53db4ccb3fc"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
