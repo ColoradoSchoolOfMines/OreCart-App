40a1c4ea8e248c9d25a1dc8cfe97068db53125b8
Tyler Wright <tyler.wright5146@gmail.com>
2024-02-28 17:27:33 -0700

    Probably this works, no more edit or add vans

--- frontend-admin/src/pages/vans/van-types.tsx
+++ frontend-admin/src/pages/vans/van-types.tsx
@@ -3,29 +3,3 @@ export interface Van {
   routeId: number;
   wheelchair: boolean;
 }
-
-export interface VanData {
-  route_id: number;
-  wheelchair: boolean;
-}
-
-export interface EditVanProps {
-  onSubmit: (data: VanData) => void; // or Promise<void> if async
-  onDelete: () => void;
-  onCancel: () => void;
-}
-
-export interface VanEditFormRef {
-  setData: (van: Van) => void;
-  clearForm: () => void;
-}
-
-export interface VanEditFormMethods {
-  setData: (van: Van) => void;
-  clearForm: () => void;
-}
-
-export interface AddVanFormProps {
-    onSubmit: (data: VanData) => void; // or Promise<void> if async
-    onCancel: () => void;
-}
--- frontend-admin/src/pages/vans/vans-page.tsx
+++ frontend-admin/src/pages/vans/vans-page.tsx
@@ -1,9 +1,6 @@
-import { Button, Modal, Paper, Stack, Title } from "@mantine/core";
-import { useQuery, useQueryClient } from "@tanstack/react-query";
-import React, { useState } from "react";
-import AddVanForm from "./add-van-form.tsx";
-import VanEditForm from "./edit-van-form.tsx";
-import { Van, VanData } from "./van-types.tsx";
+import { Paper, Stack, Title } from "@mantine/core";
+import { useQuery } from "@tanstack/react-query";
+import { Van } from "./van-types.tsx";
 import "./vans-page.scss";
 
 const baseUrl = import.meta.env.VITE_BACKEND_URL;
@@ -16,72 +13,11 @@ const fetchVans = async () => {
 };
 
 const VanPage: React.FC = () => {
-  const queryClient = useQueryClient();
   const {
     data: vans,
     isLoading,
     error,
   } = useQuery({ queryKey: ["vans"], queryFn: fetchVans });
-
-  const [currentVanId, setCurrentVanId] = useState<number>(-1);
-  const [isEditVanModalOpen, setEditVanModalOpened] = useState<boolean>(false);
-  const [isAddVanModalOpen, setIsAddVanModalOpen] = useState<boolean>(false);
-
-  const createVan = async (vanData: VanData) => {
-    // Convert the routeId to an integer
-    try {
-      await fetch(`${baseUrl}/vans/`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(vanData),
-      });
-      await queryClient.invalidateQueries({ queryKey: ["vans"] });
-      // ... Handle the response ...
-    } catch (error) {
-      console.error("Error creating van:", error);
-    }
-  };
-
-  const openVanEditForm = (vanId: number) => {
-    setCurrentVanId(vanId);
-    setEditVanModalOpened(true);
-  };
-
-  const editVanSubmit = async (vanData: VanData) => {
-    // Convert the routeId to an integer
-    try {
-      await fetch(`${baseUrl}/vans/${currentVanId}`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(vanData),
-      });
-      await queryClient.invalidateQueries({ queryKey: ["vans"] });
-      setEditVanModalOpened(false);
-      // ... Handle the response ...
-    } catch (error) {
-      console.error("Error creating van:", error);
-    }
-  };
-
-  const deleteVan = async () => {
-    try {
-      await fetch(`${baseUrl}/vans/${currentVanId}`, {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      await queryClient.invalidateQueries({ queryKey: ["vans"] });
-      setCurrentVanId(-1);
-      setEditVanModalOpened(false);
-    } catch (error) {
-      console.error("Error creating van:", error);
-    }
-  };
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>An error occurred: {(error as Error).message}</div>;
@@ -96,46 +32,11 @@ const VanPage: React.FC = () => {
               p="md"
               shadow="xs"
               className="van-card"
-              onClick={() => {
-                openVanEditForm(van.id);
-              }}
             >
               <Title order={2}>Van #{van.id}</Title>
             </Paper>
           ))}
         </div>
-
-        <Button
-          className="add-button"
-          onClick={() => setIsAddVanModalOpen(true)}
-        >
-          Add Van
-        </Button>
-        <Modal
-          centered
-          title={<Title order={2}>Add Van</Title>}
-          opened={isAddVanModalOpen}
-          onClose={() => setIsAddVanModalOpen(false)}
-        >
-          <AddVanForm
-            onSubmit={createVan}
-            onCancel={() => setIsAddVanModalOpen(false)}
-          />
-        </Modal>
-
-        <Modal
-          centered
-          title={<Title order={2}>Edit Van #{currentVanId}</Title>}
-          opened={isEditVanModalOpen}
-          onClose={() => setEditVanModalOpened(false)}
-        >
-          <VanEditForm
-            onSubmit={editVanSubmit}
-            onDelete={deleteVan}
-            onCancel={() => setEditVanModalOpened(false)}
-          />
-        </Modal>
-        {/* </dialog> */}
       </Stack>
     </main>
   );
