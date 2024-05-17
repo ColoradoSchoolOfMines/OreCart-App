f02d58f9a733179a47c98d28870149b45f3403d3
Alexander Capehart <alex@oxycblt.org>
2024-05-16 19:40:38 -0600

    hardware: fix some modem issues

--- hardware/application/src/main.cpp
+++ hardware/application/src/main.cpp
@@ -1,22 +1,23 @@
 #include <zephyr/kernel.h>
 
+#include <vector>
+
 // #include "modules/ui/route_select/RouteSelectScreen.hpp"
 // #include "modules/ui/canvas/DisplayCanvas.hpp"
 #include "modules/control/control_module.hpp"
 #include "modules/ui/ui_module.hpp"
+#include "modules/net/stack/Modem.hpp"
+
+#include "Config.hpp"
 
 int main()
 {
+    try {
+        Modem modem { DOMAIN };
+    } catch (std::runtime_error &e) {
+        printf("Modem");
+    }
     control::start();
-    // Modem modem { DOMAIN };
-    // DisplayCanvas canvas;
-    // RouteSelectScreen screen(canvas);
-    // screen.attach();
-    // Select select;
-    // select.set_text("Hello, World!");
-    // select.measure({200, 200});
-    // select.draw(canvas);
-
     ui::start();
     while (1) {
         k_sleep(K_MSEC(1000));
